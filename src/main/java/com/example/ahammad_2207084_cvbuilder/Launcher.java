ca8deee55bfd1ef24bf37300425eda3ee4ed4d56
nafisahammad <nafisahmed837@gmail.com>
2025-11-09 19:21:23 +0600

    Initial commit

---
+++ src/main/java/com/example/ahammad_2207084_cvbuilder/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.ahammad_2207084_cvbuilder;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
